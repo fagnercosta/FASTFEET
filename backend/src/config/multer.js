8022a5c0bb0b2737e6109422dff084c66949973f
root <!gown119>
2020-03-02 19:09:01 -0300

    Ajuste Multer

--- backend/src/config/multer.js
+++ backend/src/config/multer.js
@@ -5,7 +5,7 @@ import { extname, resolve } from 'path';
 
 export default {
   storage: multer.diskStorage({
-    destination: resolve(__dirname,'..','..', '..', 'tmp', 'uploads'),
+    destination: resolve(__dirname, '..', '..', 'tmp', 'uploads'),
     filename: (req, file, cb) => {
       crypto.randomBytes(16, (err, res) => {
         if (err) return cb(err);
